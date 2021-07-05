e05b15768a9a030d5730dd2e3ac2bb3280fa8530
Mindaugas <Mindaugas.sliachtovicius@gmail.com>
2021-07-05 18:27:08 +0300

    Update

--- script.js
+++ script.js
@@ -3,14 +3,42 @@ const imageWindow = document.createElement('div')
 imageWindow.id = 'imageWindow'
 document.body.appendChild(imageWindow)
 
+
 //DOM of all image tags
 const allImages = document.querySelectorAll('img')
 
+//
 allImages.forEach(image => {
-    image.addEventListener('click', event => {
+    image.addEventListener('click', () => {
         imageWindow.classList.add('active')
         const img = document.createElement('img')
         img.src = image.src
+        while(imageWindow.firstChild) {
+            imageWindow.removeChild(imageWindow.firstChild)
+        }
         imageWindow.appendChild(img)
+        
+        let btnNext = document.createElement('img')
+        btnNext.setAttribute('src', '/next.png')
+        btnNext.setAttribute('id', 'right')
+        btnNext.setAttribute('onclick', 'changeImg(1)')
+        imageWindow.appendChild(btnNext)
+
+        let btnPrev = document.createElement('img')
+        btnPrev.setAttribute('src', '/prev.png')
+        btnPrev.setAttribute('id', 'left')
+        btnPrev.setAttribute('onclick', 'changeImg(0)')
+        imageWindow.prepend(btnPrev)
     })
+    
+})
+function changeImg(changeDir) {
+
+}
+
+//removes background screen by removing it's class onclick
+imageWindow.addEventListener('click', event => {
+    if (event.target !== event.currentTarget) 
+    return
+    imageWindow.classList.remove('active')
 })
